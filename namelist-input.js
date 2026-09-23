a88a7bc73d9a0b517ffb05a5ea075d2a0d9d4927
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-23 10:23:33 +0530

    Use independent parent time-step ratios in namelist.input

--- namelist-input.js
+++ namelist-input.js
@@ -295,7 +295,7 @@ function parseStartEnd(w){
 function generateNamelistInput(){
   const n=Number(niEl('domainCount').value);
   if(domains.length!==n) throw new Error('Complete all selected WPS domains before downloading namelist.input.');
-  const w=wpsSettings(), rs=ratios(), sp=spacing(), ss=settings(), d=namelistInputValues;
+  const w=wpsSettings(), rs=ratios(), trs=typeof timeRatios==='function'?timeRatios():rs, sp=spacing(), ss=settings(), d=namelistInputValues;
   const dur=durationParts(w.startDate,w.endDate), te=parseStartEnd(w);
   let cdx=sp.dx, cdy=sp.dy, g=[];
   domains.forEach((dom,i)=>{
@@ -351,7 +351,7 @@ function generateNamelistInput(){
   L.push(' dx = '+sp.dx+','); L.push(' dy = '+sp.dy+',');
   L.push(' grid_id = '+v(domains.map((_,i)=>i+1))+','); L.push(' parent_id = '+v(domains.map((_,i)=>i===0?1:i))+',');
   L.push(' i_parent_start = '+v(is)+','); L.push(' j_parent_start = '+v(js)+',');
-  L.push(' parent_grid_ratio = '+v(rs)+','); L.push(' parent_time_step_ratio = '+v(rs)+',');
+  L.push(' parent_grid_ratio = '+v(rs)+','); L.push(' parent_time_step_ratio = '+v(trs)+',');
   L.push(' feedback = '+niEl('niFeedback').value+','); L.push(' smooth_option = '+niEl('niSmoothOption').value+','); L.push('/','');
 
   L.push('&physics');
